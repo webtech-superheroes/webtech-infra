5b7e24c809163dfaa431b821889e4b0dcf28ef0a
Eduard Budacu <eduard.budacu@gmail.com>
2021-03-27 10:12:46 +0000

    updated templates and added script to stop all workspaces

--- create-workspace.sh
+++ create-workspace.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-USERNAME=""
-PASSWORD=""
-SERVER_IP=""
+USERNAME="familyguy"
+PASSWORD="web19"
+SERVER_IP="127.0.1.42"
+REPO="https://github.com/ionalexandru99/WebTech"
 
 cd /var/www/workspaces
 cp -R /var/www/workspaces/template /var/www/workspaces/$USERNAME
 chown -R 1000:1000 /var/www/workspaces/$USERNAME/project
+
+sed -i -e "s/{username}/$USERNAME/g" /var/www/workspaces/$USERNAME/project/README.md
+
+git clone $REPO /var/www/workspaces/$USERNAME/project/app
+chown -R 1000:1000 /var/www/workspaces/$USERNAME/project/app
 
 cd /var/www/workspaces/$USERNAME
 sed -i -e "s/{server_ip}/$SERVER_IP/g" /var/www/workspaces/$USERNAME/docker-compose.yml
